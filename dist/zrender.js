cfb8549e4ac126ff116effc74cdfb14ba758bd90
damoqiongqiu <damoqiongqiu@126.com>
2020-02-05 14:18:59 +0800

    refactor Animator and Clip

--- dist/zrender.js
+++ dist/zrender.js
@@ -3586,106 +3586,6 @@ var easing = {
     }
 };
 
-/**
- * 动画片段
- * 图元上存在很多种属性，在动画过程中，可能会有多种属性同时发生变化，
- * 每一种属性天然成为一条动画轨道，把这些轨道上的变化过程封装在很多 Clip 实例中。
- * 
- * @config target 动画对象，可以是数组，如果是数组的话会批量分发onframe等事件
- * @config life(1000) 动画时长
- * @config delay(0) 动画延迟时间
- * @config loop(true)
- * @config gap(0) 循环的间隔时间
- * @config onframe
- * @config easing(optional)
- * @config ondestroy(optional)
- * @config onrestart(optional)
- *
- * TODO pause
- */
-
-function Clip(options) {
-    this._target = options.target;
-    this._lifeTime = options.lifeTime || 1000;
-    this._delay = options.delay || 0;
-    this._initialized = false;
-    this.loop = options.loop == null ? false : options.loop;
-    this.gap = options.gap || 0;
-    this.easing = options.easing || 'Linear';
-    this.onframe = options.onframe;
-    this.ondestroy = options.ondestroy;
-    this.onrestart = options.onrestart;
-    this._pausedTime = 0;
-    this._paused = false;
-}
-
-Clip.prototype = {
-
-    constructor: Clip,
-
-    step: function (globalTime, deltaTime) {
-        // Set startTime on first step, or _startTime may has milleseconds different between clips
-        // PENDING
-        if (!this._initialized) {
-            this._startTime = globalTime + this._delay;
-            this._initialized = true;
-        }
-
-        if (this._paused) {
-            this._pausedTime += deltaTime;
-            return;
-        }
-
-        var percent = (globalTime - this._startTime - this._pausedTime) / this._lifeTime;
-        // 还没开始
-        if (percent < 0) {
-            return;
-        }
-        percent = Math.min(percent, 1);
-
-        var easing$$1 = this.easing;
-        var easingFunc = typeof easing$$1 === 'string' ? easing[easing$$1] : easing$$1;
-        var schedule = typeof easingFunc === 'function'
-            ? easingFunc(percent)
-            : percent;
-
-        this.fire('frame', schedule);
-
-        // 结束或者重新开始周期
-        // 抛出而不是直接调用事件直到 stage.update 后再统一调用这些事件
-        // why?
-        if (percent === 1) {
-            if (this.loop) {
-                this.restart(globalTime);
-                return 'restart';
-            }
-            return 'destroy';
-        }
-        return null;
-    },
-
-    restart: function (globalTime) {
-        var remainder = (globalTime - this._startTime - this._pausedTime) % this._lifeTime;
-        this._startTime = globalTime - remainder + this.gap;
-        this._pausedTime = 0;
-    },
-
-    fire: function (eventType, arg) {
-        eventType = 'on' + eventType;
-        if (this[eventType]) {
-            this[eventType](this._target, arg);
-        }
-    },
-
-    pause: function () {
-        this._paused = true;
-    },
-
-    resume: function () {
-        this._paused = false;
-    }
-};
-
 // Simple LRU cache use doubly linked list
 // @module zrender/core/LRU
 
@@ -4435,10 +4335,23 @@ var colorUtil = (Object.freeze || Object)({
 });
 
 /**
- * Animator 是动画片段 Clip 的管理器，负责创建、维护 Clip，Animator 可以看成一组 Clip 的集合。
+ * 动画片段
+ * 图元上存在很多种属性，在动画过程中，可能会有多种属性同时发生变化，
+ * 每一种属性天然成为一条动画轨道，把这些轨道上的变化过程封装在很多 Clip 实例中。
  * 
- * @module echarts/animation/Animator
+ * @config target 动画对象，可以是数组，如果是数组的话会批量分发onframe等事件
+ * @config life(1000) 动画时长
+ * @config delay(0) 动画延迟时间
+ * @config loop(true)
+ * @config gap(0) 循环的间隔时间
+ * @config onframe
+ * @config easing(optional)
+ * @config ondestroy(optional)
+ * @config onrestart(optional)
+ *
+ * TODO pause
  */
+
 /**
  * 创建片段
  * @param {*} animator 
@@ -4448,10 +4361,10 @@ var colorUtil = (Object.freeze || Object)({
  * @param {*} propName 
  * @param {*} forceAnimate 
  */
-function createClip(animator, easing, oneTrackDone, keyframes, propName, forceAnimate) {
+function calculateParams(animator, easing$$1, oneTrackDone, keyframes, propName, forceAnimate) {
     var getter = animator._getter;
     var setter = animator._setter;
-    var useSpline = easing === 'spline';
+    var useSpline = easing$$1 === 'spline';
 
     var kfLength = keyframes.length;
     if (!kfLength) {
@@ -4635,19 +4548,122 @@ function createClip(animator, easing, oneTrackDone, keyframes, propName, forceAn
         }
     };
 
-    var clip = new Clip({
+    // var clip = new Clip({
+    //     target: animator._target,
+    //     lifeTime: trackMaxTime,
+    //     loop: animator._loop,
+    //     delay: animator._delay,
+    //     onframe: onframe,
+    //     ondestroy: oneTrackDone,
+    //     easing: (easing && easing !== 'spline')?easing:'Linear'
+    // });
+
+    // return clip;
+    let options={
         target: animator._target,
         lifeTime: trackMaxTime,
         loop: animator._loop,
         delay: animator._delay,
         onframe: onframe,
         ondestroy: oneTrackDone,
-        easing: (easing && easing !== 'spline')?easing:'Linear'
-    });
-
-    return clip;
+        easing: (easing$$1 && easing$$1 !== 'spline')?easing$$1:'Linear'
+    };
+    return options;
 }
 
+function Clip(animator, easing$$1, oneTrackDone, keyframes, propName, forceAnimate) {
+    let options=calculateParams(animator, easing$$1, oneTrackDone, keyframes, propName, forceAnimate);
+    if(!options){
+        return null;
+    }
+    
+    this._target = options.target;
+    this._lifeTime = options.lifeTime || 1000;
+    this._delay = options.delay || 0;
+    this._initialized = false;
+    this.loop = options.loop == null ? false : options.loop;
+    this.gap = options.gap || 0;
+    this.easing = options.easing || 'Linear';
+    this.onframe = options.onframe;
+    this.ondestroy = options.ondestroy;
+    this.onrestart = options.onrestart;
+
+    this._pausedTime = 0;
+    this._paused = false;
+}
+
+Clip.prototype = {
+
+    constructor: Clip,
+
+    step: function (globalTime, deltaTime) {
+        // Set startTime on first step, or _startTime may has milleseconds different between clips
+        // PENDING
+        if (!this._initialized) {
+            this._startTime = globalTime + this._delay;
+            this._initialized = true;
+        }
+
+        if (this._paused) {
+            this._pausedTime += deltaTime;
+            return;
+        }
+
+        var percent = (globalTime - this._startTime - this._pausedTime) / this._lifeTime;
+        // 还没开始
+        if (percent < 0) {
+            return;
+        }
+        percent = Math.min(percent, 1);
+
+        var easing$$1 = this.easing;
+        var easingFunc = typeof easing$$1 === 'string' ? easing[easing$$1] : easing$$1;
+        var schedule = typeof easingFunc === 'function'
+            ? easingFunc(percent)
+            : percent;
+
+        this.fire('frame', schedule);
+
+        // 结束或者重新开始周期
+        // 抛出而不是直接调用事件直到 stage.update 后再统一调用这些事件
+        // why?
+        if (percent === 1) {
+            if (this.loop) {
+                this.restart(globalTime);
+                return 'restart';
+            }
+            return 'destroy';
+        }
+        return null;
+    },
+
+    restart: function (globalTime) {
+        var remainder = (globalTime - this._startTime - this._pausedTime) % this._lifeTime;
+        this._startTime = globalTime - remainder + this.gap;
+        this._pausedTime = 0;
+    },
+
+    fire: function (eventType, arg) {
+        eventType = 'on' + eventType;
+        if (this[eventType]) {
+            this[eventType](this._target, arg);
+        }
+    },
+
+    pause: function () {
+        this._paused = true;
+    },
+
+    resume: function () {
+        this._paused = false;
+    }
+};
+
+/**
+ * Animator 是动画片段 Clip 的管理器，负责创建、维护 Clip，Animator 可以看成一组 Clip 的集合。
+ * 
+ * @module echarts/animation/Animator
+ */
 /**
  * @alias module:zrender/animation/Animator
  * @constructor
@@ -4781,8 +4797,8 @@ Animator.prototype = {
             if (!this._tracks.get(propName)) {
                 return;
             }
-            var clip = createClip(
-                this, 
+            var clip = new Clip(
+                this,
                 easing, 
                 oneTrackDone,
                 this._tracks.get(propName),
@@ -4797,7 +4813,7 @@ Animator.prototype = {
         });
 
         // Add during callback on the last clip
-        if (lastClip) {
+        if (lastClip&&isFunction(lastClip.onframe)) {
             var oldOnFrame = lastClip.onframe;
             lastClip.onframe = function (target, percent) {
                 oldOnFrame(target, percent);
@@ -10616,7 +10632,10 @@ Painter.prototype = {
 };
 
 /**
- * 动画主类, 调度和管理所有动画控制器
+ * Animation manager, global singleton, controls all the animation process.
+ * Each ZRender instance has a AnimationMgr instance.
+ * 
+ * 动画管理器，全局单例，控制和调度所有动画过程。
  * 每个 zrender 实例中会持有一个 AnimationMgr 实例。
  * 
  * @module zrender/animation/AnimationMgr
