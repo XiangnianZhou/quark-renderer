d6a2b2fd08894990122ddc7141daebcc16d05684
damoqiongqiu <damoqiongqiu@126.com>
2020-03-14 19:14:54 +0800

    temp stash

--- src/graphic/link/LinkControl.js
+++ src/graphic/link/LinkControl.js
@@ -44,35 +44,28 @@ export default class LinkControl {
     }
 
     _calcParameters(){
+        //calculate the position of the link control
+        let linkPosition=[0,0];
         let point0=[0,0];
         let point1=[0,0];
-
         if(this.name==='START'){
             point0 = this.el.pointAt(0);
             point1 = this.el.pointAt(0.1);
+            linkPosition=point0;
         }else if(this.name==='END'){
-            point0 = this.el.pointAt(1);
-            point1 = this.el.pointAt(0.9);
+            point0 = this.el.pointAt(0.9);
+            point1 = this.el.pointAt(1);
+            linkPosition=point1;
         }
-
-        let cosp2=matrixUtil.cosp2(point0,point1);//FIXME: there are some errors here
+        let cosp2=matrixUtil.cosp2(point0,point1);
         let sinp2=matrixUtil.sinp2(point0,point1);
+        linkPosition[0]=linkPosition[0]-this.radius*cosp2;
+        linkPosition[1]=linkPosition[1]-this.radius*sinp2;
+        //calculate end
 
-        if(cosp2>0){
-            point0[0]=point0[0]-this.radius/2;
-        }else{
-            point0[0]=point0[0]+this.radius/2;
-        }
-
-        if(sinp2>0){
-            point0[1]=point0[1]-this.radius/2;
-        }else{
-            point0[1]=point0[1]+this.radius/2;
-        }
-        
-        this.center=point0;
+        this.center=linkPosition;
         this.translate=[this.el.position[0],this.el.position[1]];
-        return point0;
+        return linkPosition;
     }
 
     isHover(x,y){
