37e98127d92e0efee5a2388217cbf15892fd279d
damoqiongqiu <damoqiongqiu@126.com>
2020-04-05 12:41:15 +0800

    refactor comments in GeoLine and GeoPoint

--- src/geometric/GeoLine.js
+++ src/geometric/GeoLine.js
@@ -3,12 +3,12 @@ import {mathMin, mathMax, mathAbs, mathSqrt, mathPow} from '../utils/constants';
 
 /**
  * @class qrenderer.geometric.GeoLine
- * 
+ * A geometrically line, invisible, no dimension, just used for mathematical operations.
+ * This implementation is improved from http://diagramo.com/ .
  * 
  *  
- * 几何学意义上的直线，它不可见，没有宽度，用来进行数学运算。
+ * 几何学意义上的直线，它不可见，没有宽度，用来进行数学运算。此实现从 diagramo 改进而来：http://diagramo.com/ 。
  * 
- * @author 大漠穷秋 <damoqiongqiu@126.com>
  * @docauthor 大漠穷秋 <damoqiongqiu@126.com>
  */
 export default class GeoLine{
@@ -18,7 +18,12 @@ export default class GeoLine{
     }
 
     /**
-     * Creates a {GeoLine} out of JSON parsed object
+     * @static
+     * @method load
+     * Creates a {GeoLine} out of JSON parsed object.
+     * 
+     * 
+     * 从 JSON 对象创建直线。
      * @param {JSONObject} o - the JSON parsed object
      * @return {GeoLine} a newly constructed GeoLine
      */
@@ -31,8 +36,13 @@ export default class GeoLine{
     }
 
     /** 
+     * @method constants
      * Tests to see if a point belongs to this line (not as infinite line but more like a segment)
      * Algorithm: Compute line's equation and see if (x, y) verifies it.
+     * 
+     * 
+     * 测试某个点是否位于直线上（这里不是数学意义上的无线延长直线，而是线段）。
+     * 算法：计算斜率，看(x,y)点是否位于线段上。
      * @param {Number} x - the X coordinates
      * @param {Number} y - the Y coordinates
      */
@@ -57,7 +67,12 @@ export default class GeoLine{
     }
 
     /**
-     * See if we are near a {GeoLine} by a certain radius (also includes the extremities into computation)
+     * @method near
+     * See if we are near a {GeoLine} by a certain radius (also includes the extremities into computation).
+     * 
+     * 
+     * 测试某个点是否在某个角度上接近 {GeoLine} （断点也计算在内）。
+     * 
      * @param {Number} x - the x coordinates
      * @param {Number} y - the y coordinates
      * @param {Number} radius - the radius to search for
@@ -107,7 +122,11 @@ export default class GeoLine{
     }
 
     /**
-     * we need to create a new array each time, or we will affect the actual shape
+     * @method getPoints
+     * Get an arry composed by the start point and end point of the line.
+     * 
+     * 
+     * 获取端点构成的数组。
      */
     getPoints(){
         let points = [];
@@ -117,7 +136,11 @@ export default class GeoLine{
     }
 
     /**
-     * Return the {GeoPoint} corresponding the t certain t value
+     * @method getPoint
+     * Return the {GeoPoint} corresponding the t certain t value.
+     * 
+     * 
+     * 获取指定百分比上的点，参数 t 是百分比。
      * @param {Number} t the value of parameter t, where t in [0,1], t is like a percent
      */
     getPoint(t){
@@ -126,11 +149,18 @@ export default class GeoLine{
         return new GeoPoint(xp, yp);
     }
 
+    /**
+     * @method clone
+     */
     clone(){
         let ret = new GeoLine(this.startPoint.clone(), this.endPoint.clone());
         return ret;
     }
 
+    /**
+     * @equals
+     * @param {*} anotherLine 
+     */
     equals(anotherLine){
         if(!(anotherLine instanceof GeoLine)){
             return false;
@@ -138,6 +168,9 @@ export default class GeoLine{
         return this.startPoint.equals(anotherLine.startPoint)&& this.endPoint.equals(anotherLine.endPoint)
     }
 
+    /**
+     * @method toString
+     */
     toString(){
         return 'line(' + this.startPoint + ',' + this.endPoint + ')';
     }
